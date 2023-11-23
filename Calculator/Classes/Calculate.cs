6846355a362686711566eedef141c44989c1e4b8
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2023-11-23 22:27:05 +0500

    Add project files.

---
+++ Calculator/Classes/Calculate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Classes
+{
+    internal class Calculate
+    {
+        string name;
+        char item;
+        double firstNumber, secondNumber;
+
+        public Calculate(string name, char item, double firstNumber, double secondNumber)
+        {
+            this.name = name;
+            this.item = item;
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+        }
+
+        public void Do()
+        {
+            switch (item)
+            {
+                case '+':
+                    Addition();
+                    break;
+                case '-':
+                    Subtraction();
+                    break;
+                case '*':
+                    Multiplication();
+                    break;
+                case '/':
+                    Division();
+                    break;
+                default:
+                    Console.WriteLine("Inserted action was error");
+                    break;
+            }
+        }
+
+        public void Addition()
+        {
+            double result = firstNumber + secondNumber;
+            Console.WriteLine($"\nName: {name} \n{firstNumber} + {secondNumber} = {result}");
+        }
+
+        public void Subtraction()
+        {
+            double result = firstNumber - secondNumber;
+            Console.WriteLine($"\nName: {name} \n{firstNumber} - {secondNumber} = {result}");
+        }
+
+        public void Multiplication()
+        {
+            double result = firstNumber * secondNumber;
+            Console.WriteLine($"\nName: {name} \n{firstNumber} * {secondNumber} = {result}");
+        }
+
+        public void Division()
+        {
+            double result = firstNumber / secondNumber;
+            Console.WriteLine($"\nName: {name} \n{firstNumber} / {secondNumber} = {result}");
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using Calculator.Classes;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Function [ +, -, *, / ]: ");
+            char item = Convert.ToChar(Console.ReadLine());
+            Console.Write("firstNumber: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("secondNumber: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+
+            Calculate calculate = new Calculate(name, item, firstNumber, secondNumber);
+            calculate.Do();
+        }
+    }
+}
